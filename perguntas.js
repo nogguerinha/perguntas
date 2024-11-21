9285b39657abe259d9a5f806f31280d078420102
nogguerinha <muka34553@gmail.com>
2024-11-21 14:32:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Fundação',
+    'Em que ano a Ponte Preta foi fundada?',
+    '1900'
+)
+
+criaCartao(
+    'Títulos',
+    'Quantos títulos a Ponte Preta tem?',
+    '0'
+)
+
+criaCartao(
+    'Artilharia',
+    'Qual o maior artilheiro da Ponte Preta?',
+    'Dicá'
+)
+
+criaCartao(
+    'Mascote',
+    'Qual o mascote da Ponte Preta?',
+    'Macaca',
+
+)
